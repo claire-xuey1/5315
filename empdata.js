47a0eb2e8784b244a0616c139012b3731153c42e
claire-xuey1 <xuey1@seattleu.edu>
2019-04-21 00:13:27 -0700

    hw3 files

---
+++ empdata.js
@@ -0,0 +1,109 @@
+{
+  "data": [
+    {
+      "sudentID": "12345",
+      "student_name": "Si Li",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "1"
+    },
+    {
+      "sudentID": "12333",
+      "student_name": "Ming Li",
+      "student_major": "MSBA",
+      "student_hobby": "Volleyball",
+      "groupID": "1"
+    },
+    {
+      "sudentID": "12385",
+      "student_name": "Han Li",
+      "student_major": "MSBA",
+      "student_hobby": "Hiking",
+      "groupID": "1"
+    },
+    {
+      "sudentID": "11335",
+      "student_name": "Si Zhang",
+      "student_major": "CS",
+      "student_hobby": "Outdoors",
+      "groupID": "2"
+    },
+    {
+      "sudentID": "11325",
+      "student_name": "Jack",
+      "student_major": "Philosophy",
+      "student_hobby": "Dancing",
+      "groupID": "2"
+    },
+    {
+      "sudentID": "13335",
+      "student_name": "Kitty",
+      "student_major": "Communication",
+      "student_hobby": "Cooking",
+      "groupID": "2"
+    },
+    {
+      "sudentID": "12349",
+      "student_name": "Thomas",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "3"
+    },
+    {
+      "sudentID": "12391",
+      "student_name": "Cathy",
+      "student_major": "CS",
+      "student_hobby": "football",
+      "groupID": "3"
+    },
+    {
+      "sudentID": "13645",
+      "student_name": "Jullian",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "3"
+    },
+    {
+      "sudentID": "12347",
+      "student_name": "Tracy",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "4"
+    },
+    {
+      "sudentID": "12344",
+      "student_name": "Grace",
+      "student_major": "CS",
+      "student_hobby": "football",
+      "groupID": "4"
+    },
+    {
+      "sudentID": "12375",
+      "student_name": "Danniel",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "4"
+    },
+    {
+      "sudentID": "12351",
+      "student_name": "Jason",
+      "student_major": "MSBA",
+      "student_hobby": "football",
+      "groupID": "5"
+    },
+    {
+      "sudentID": "12323",
+      "student_name": "Gaby",
+      "student_major": "MSBA",
+      "student_hobby": "Swimming",
+      "groupID": "5"
+    },
+    {
+      "sudentID": "12311",
+      "student_name": "Keith",
+      "student_major": "MSBA",
+      "student_hobby": "Running",
+      "groupID": "5"
+    }
+  ]
+}
